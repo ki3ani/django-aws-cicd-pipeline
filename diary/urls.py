2814248d42a309174d096451f32bb716c89562e3
ki3ani <kimanim68@gmail.com>
2024-01-28 18:01:13 +0300

    pushinfix

--- diary/urls.py
+++ diary/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('library.urls')), # new
+    path('/', include('library.urls')), # new
 ]
--- library/urls.py
+++ library/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('books/', views.BookView.as_view(), name='book_list'),
     path('books/<int:pk>', views.BookDetail.as_view(), name='book_detail'), # new
     path('debug/', debug_view, name='debug_view'),
-    path('', health_check, name='health_check'),
+    path('/health-check', health_check, name='health_check'),
 ]
